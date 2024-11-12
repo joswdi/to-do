e8494d49e82cf8bf295b2468e70b9667c622e4ad
joswdi <kovalchuksasha2207@gmail.com>
2024-11-12 18:03:41 +0100

    .

---
+++ to-do/src/App.js
@@ -0,0 +1,66 @@
+import "./App.css";
+import { TextField, Button, Paper } from "@mui/material";
+import ToDoItem from "./components/to-do-item/ToDoItem";
+import { useState } from "react";
+
+function App() {
+  const [todoList, setToDo] = useState([
+    {
+      name: "First task",
+      id: 1,
+      checked: true,
+    },
+    {
+      name: "Second task",
+      id: 2,
+      checked: true,
+    },
+    {
+      name: "Third task",
+      id: 3,
+      checked: false,
+    }
+  ])
+
+  const addNewTask = () => {
+    if (document.querySelector("#filled-basic").value) {
+    const newToDo = [...todoList]
+    const newName = document.querySelector("#filled-basic").value
+    newToDo.push({id: todoList.length + 1, name: newName })
+    setToDo(newToDo)
+    document.querySelector("#filled-basic").value = ""
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>to do</h1>
+      <Paper
+        sx={{
+          p: "16px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          mb: "20px",
+          width: "500px",
+        }}
+        elevation={7}
+      >
+        <TextField
+          sx={{ mr: "10px", width: "400px" }}
+          id="filled-basic"
+          label="Do name"
+          variant="filled"
+        />
+        <Button onClick={addNewTask} variant="contained">Save</Button>
+      </Paper>
+      <Paper elevation={7} sx={{padding: "10px", height: "400px", overflowY: "scroll"}}>
+      {todoList.map((todo) => {
+        return <ToDoItem setToDo={setToDo} todo={todo} />
+      })}
+      </Paper>
+    </div>
+  );
+}
+
+export default App;
---
+++ to-do/src/components/to-do-item/ToDoItem.js
@@ -0,0 +1,47 @@
+import { TextField, Paper, Checkbox, IconButton, Tooltip } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const ToDoItem = ({ todo, setToDo }) => {
+  const { name, id, checked } = todo;
+
+  const deleteTask = (e) => {
+    setToDo((previousState) => {
+      const newState = previousState.filter((el) => el.id !== el.target.id)
+      return newState
+    });
+  };
+
+  return (
+    <Paper
+      sx={{
+        p: "16px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "400px",
+        marginBottom: "20px",
+      }}
+      elevation={7}
+    >
+      <Checkbox id={id} checked={checked} />
+      <TextField
+        disabled
+        value={name}
+        id="outlined-disabled"
+        label="To-do"
+        variant="outlined"
+      />
+      <Tooltip title="Delete task">
+        <IconButton
+          onClick={deleteTask}
+          id={id}
+          aria-label="delete"
+          size="large"
+        >
+          <DeleteIcon id={id} />
+        </IconButton>
+      </Tooltip>
+    </Paper>
+  );
+};
+export default ToDoItem;
